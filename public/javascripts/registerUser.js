69a86e69e68828a100fa958e4fc80d330b3c0668
karthik <raokarthik83@gmail.com>
2022-03-20 09:25:00 +0530

    Tested user creation

---
+++ public/javascripts/registerUser.js
@@ -0,0 +1,34 @@
+function registerUser() {
+
+    //Get username and password from UI
+    let name = document.getElementById("name").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let address = document.getElementById("address").value;
+    let dob = document.getElementById("dob").value;
+    let mobileNumber = document.getElementById("mobileNumber").value;
+
+    console.log(username);
+    console.log(password);
+
+    let userObject = {
+        name: name,
+        username: username,
+        password: password,
+        address: address,
+        dob:dob,
+        mobileNumber: mobileNumber
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:27018/userdb/users',
+        data: JSON.stringify(userObject),
+        success: function(data) { 
+            alert("User created");
+            //Redirect to list of users
+        },
+        contentType: "application/json",
+        dataType: 'json'
+    });
+}
